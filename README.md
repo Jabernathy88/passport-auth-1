9a2bbb5f3dd5ab4e86dc6dc864df2177e183ba6b
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-18 14:01:48 -0400

    Successfully saved encrypted password User on localhost.

---
+++ README.md
@@ -0,0 +1,1 @@
+Based on [Node Passport](https://www.youtube.com/watch?v=6FOq4cUdH8k&t=9s) tutorial by Brad Traversy.
--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+require("dotenv").config()
+
 const express = require('express');
-// const expressLayouts = require('express-ejs-layouts');
-// const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 // const passport = require('passport');
 // const flash = require('connect-flash');
 // const session = require('express-session');
-
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// DB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true 
+})
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const connection = mongoose.connection
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully')
+})
+
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)
+})
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Built-in Bodyparser
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`!! Passport server started on port ${PORT} !!`));
