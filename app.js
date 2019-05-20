ab561f219ae225d752c87cc72bbc2ba86a3b78ad
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-20 14:28:22 -0400

    Complete login and logout functions.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require("dotenv").config()
 
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-const app = express();
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const flash = require('connect-flash')
+const session = require('express-session')
+const app = express()
+
+// Passport Config
+require('./config/passport')(passport)
 
 // DB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -18,17 +21,17 @@ connection.on('connected', () => {
   console.log('Mongoose Connected Successfully')
 })
 
-// If the connection throws an error
+// Handle DB connection error
 connection.on('error', (err) => {
   console.log('Mongoose default connection error: ' + err)
 })
 
 // EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
 
 // Built-in Bodyparser
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // Express session
 app.use(
@@ -37,27 +40,27 @@ app.use(
     resave: true,
     saveUninitialized: true
   })
-);
+)
 
-// // Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Connect flash
-app.use(flash());
+app.use(flash())
 
 // Global variables
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
 
 // Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
+app.use('/', require('./routes/index.js'))
+app.use('/users', require('./routes/users.js'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log(`!! Passport server started on port ${PORT} !!`));
+app.listen(PORT, console.log(`!! Passport server started on port ${PORT} !!`))
